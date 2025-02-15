76effb0e2cfe54babac1dfb2c22fb58c3cf85185
prank-vish <prankvish@gmail.com>
2025-02-15 13:55:43 +0530

    FINAL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PRODIGY_WB_04/',
+  base: '/PRODIGY_WD_04/',
 })
